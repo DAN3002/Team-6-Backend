f710ee4216ece6a0a525d7dbffa69f673ca9b06e
Nguyễn Đình Anh <dinhanh300229@gmail.com>
2023-03-06 16:50:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = config.port || 7777;
+const port = 3000;
 const path = require('path');
 
 
@@ -31,3 +31,5 @@ app.listen(port, () =>
 {
     console.log(`-- Server running on port ${port}`);
 });
+
+module.exports = app;
